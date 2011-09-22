2c213d05d310cb5a1daa15c325128debc20ed1a8
Mikko Koponen <mikko.koponen@reaktor.fi>
2011-09-22 12:34:30 +0300

    Update sbt from 0.7.7 to 0.10.1. Update scala from 2.7.7 to 2.9.1.

---
+++ project/build.scala
@@ -0,0 +1,30 @@
+import sbt._
+
+import Keys._
+import AndroidKeys._
+
+object General {
+  val settings = Defaults.defaultSettings ++ Seq (
+    name := "my-log",
+    version := "0.7",
+    scalaVersion := "2.9.1",
+    platformName in Android := "android-7"
+  )
+
+  lazy val fullAndroidSettings =
+    General.settings ++
+    AndroidProject.androidSettings ++
+    TypedResources.settings ++
+    AndroidMarketPublish.settings ++ Seq (
+      keyalias in Android := "change-me",
+      libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+    )
+}
+
+object AndroidBuild extends Build {
+  lazy val main = Project (
+    "my-log",
+    file("."),
+    settings = General.fullAndroidSettings
+  )
+}
